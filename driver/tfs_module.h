6b2852f6a2a2c54bf92b5bfa9366ecf14904af94
shoily <shoily@6ea2a521-684e-4afd-b4ea-8fbe0e0ccdb0>
2010-08-10 15:39:09 +0000

    First import - moved source code from root dir
    git-svn-id: svn://localhost/tmp/tfs/trunk@6 6ea2a521-684e-4afd-b4ea-8fbe0e0ccdb0

---
+++ driver/tfs_module.h
@@ -0,0 +1,42 @@
+#ifndef _TFS_MODULE_H
+#define _TFS_MODULE_H
+
+
+#include "tfs.h"
+
+#include <linux/fs.h>
+#include <linux/buffer_head.h>
+#include <linux/mutex.h>
+
+struct tfs_sb_info
+{
+  struct tfs_super_block *super_block;
+  struct buffer_head *bh;
+  struct mutex inode_bitmap_mutex;
+  struct mutex data_bitmap_mutex;
+  struct mutex inode_buffer_mutex;
+};
+
+struct tfs_inode_info
+{
+  unsigned int data_blocks[TFS_DATA_BLOCKS_PER_INODE];
+  struct inode inode;
+};
+
+#define TFS_INODE(vfs_inode) container_of(vfs_inode, struct tfs_inode_info, inode)
+
+struct inode *tfs_inode_get(struct super_block *sb, int ino);
+void tfs_destroy_inode(struct inode *inode);
+int tfs_write_begin(struct file *file, struct address_space *mapping,
+		    loff_t pos, unsigned len, unsigned flags, 
+		    struct page **pagep, void **fsdata);
+int __tfs_write_begin(struct file *file, struct address_space *mapping,
+		      loff_t pos, unsigned len, unsigned flags,
+		      struct page **pagep, void **fsdata);
+int tfs_commit_write(struct page *page, loff_t pos, unsigned len);
+loff_t tfs_llseek(struct file *file, loff_t offset, int origin);
+int tfs_fsync(struct file *file, struct dentry *dentry, int datasync);
+int tfs_getblocks(struct inode *inode, sector_t iblock, struct buffer_head *bh_result, int create);
+int tfs_sync_inode(struct inode *inode);
+
+#endif
